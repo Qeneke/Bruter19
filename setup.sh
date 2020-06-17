d0f83cf0e9071ed236f2ba4f1de7753d3c973550
AzizKpln <azizkaplan@protonmail.ch>
2020-06-17 10:55:52 -0400

    bruter19

---
+++ setup.sh
@@ -0,0 +1,54 @@
+if [ $(id -u) -ne 0 ]; then
+	echo "This script must be ran as root"
+	exit 1
+fi
+sudo apt install python3 -y
+sudo apt install python3-pip -y
+sudo pip3 install clint
+sudo apt-get update && sudo apt-get install python3-pip -y
+sudo apt-get install chromium -y
+pip3 install colored
+sudo pip3 install colored
+wget https://chromedriver.storage.googleapis.com/81.0.4044.69/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+cp chromedriver path/
+rm chromedriver*
+sudo apt-get install figlet
+pip3 install certifi
+pip3 install -r requirements.txt
+sudo pip3 install certifi
+sudo pip3 install -r requirements.txt
+
+# Ensure we are being ran as root
+if [ $(id -u) -ne 0 ]; then
+	echo "This script must be ran as root"
+	exit 1
+fi
+
+# For upgrades and sanity check, remove any existing i2p.list file
+rm -f /etc/apt/sources.list.d/i2p.list
+
+# Compile the i2p ppa
+echo "deb http://deb.i2p2.no/ unstable main" > /etc/apt/sources.list.d/i2p.list # Default config reads repos from sources.list.d
+wget https://geti2p.net/_static/i2p-debian-repo.key.asc -O /tmp/i2p-debian-repo.key.asc # Get the latest i2p repo pubkey
+apt-key add /tmp/i2p-debian-repo.key.asc # Import the key
+rm /tmp/i2p-debian-repo.key.asc # delete the temp key
+apt-get update # Update repos
+
+if [[ -n $(cat /etc/os-release |grep kali) ]]
+then
+	apt-get install libservlet3.0-java 
+	wget http://ftp.us.debian.org/debian/pool/main/j/jetty8/libjetty8-java_8.1.16-4_all.deb
+	dpkg -i libjetty8-java_8.1.16-4_all.deb # This should succeed without error
+	apt-get install libecj-java libgetopt-java libservlet3.0-java glassfish-javaee ttf-dejavu i2p i2p-router libjbigi-jni #installs i2p and other dependencies
+	apt-get -f install # resolves anything else in a broken state
+fi
+
+apt-get install -y i2p-keyring #this will ensure you get updates to the repository's GPG key
+apt-get install -y secure-delete tor i2p # install dependencies, just in case
+
+# Configure and install the .deb
+dpkg-deb -b kali-anonsurf-deb-src/ kali-anonsurf.deb # Build the deb package
+dpkg -i kali-anonsurf.deb || (apt-get -f install && dpkg -i kali-anonsurf.deb) # this will automatically install the required packages
+
+exit 0
